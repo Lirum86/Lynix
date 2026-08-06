87dfd28b7c61f0b886dca37e66d6379ea26b6043
Lirum86 <lirum.shop@gmail.com>
2026-08-06 17:19:02 +0200

    Update Lynix.lua

--- Lynix.lua
+++ Lynix.lua
@@ -44,6 +44,7 @@ local gameScripts = {
     ['136801880565837'] = 'FPSFlick.lua',
     ['109265479748625'] = 'RepairaCar.lua',
     ['127822680964493'] = 'PrisonLiftClash.lua',
+    ['72532006226238'] = 'LeaveCollect.lua',
     ['77499336428083'] = 'DrawARaft.lua',
     ['96469185605358'] = 'AgeEvolutionTycoon.lua'
 }
